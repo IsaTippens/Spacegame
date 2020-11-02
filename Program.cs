c938179904e3ec8f70cb37595b3a1efabe921c76
Isa Tippens <isatippens2@gmail.com>
2020-11-02 09:47:47 +0200

    Added global constants for screen dimensions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
-﻿namespace Spacegame
+﻿using static Spacegame.Utils;
+namespace Spacegame
 {
     static class Program
     {
-        const int screenWidth = 1366;
-        const int screenHeight = 768;
         public static void Main()
         {
-            using (var game = new TestGame()) {
+            using (var game = new TestGame(screenWidth, screenHeight)) {
                 game.Run();
             }
         }
--- Utils.cs
+++ Utils.cs
@@ -4,5 +4,8 @@ namespace Spacegame
     public static class Utils
     {
         public static Random RNG = new Random();
+
+        public static int screenWidth = 1280;
+        public static int screenHeight = 720;
     }
 }
